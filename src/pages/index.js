5fb5f6f183aaa8ca134f0101f3338824b2c086df
AGNIESZKA ZALEWSKA <agnieszka.e.zalewska@gmail.com>
2021-05-10 11:22:10 +0200

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,7 +19,7 @@ export default function Home() {
           <div className="hero-container">
             <div className="hero-text">
               <h1>tasty recipes</h1>
-              <h4>no fluff, just recipes</h4>
+              <h4>all the recipes you ever wanted</h4>
             </div>
           </div>
         </header>
